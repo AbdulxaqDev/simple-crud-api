a52a8f01e950ca620a82a12e81b59d81824e780b
AbdulxaqDev <abdulkhak8tursunov@gmail.com>
2024-02-12 23:27:45 +0300

    feat: add uuid id generator and validator

--- src/utils/user.util.ts
+++ src/utils/user.util.ts
@@ -1,8 +1,20 @@
-import { tUser } from "src/types/user";
-import db from "src/db";
+import { validate, v4 as uuidv4 } from "uuid";
+import { tUser } from "../types/user";
+import db from "../db";
 
 export function isUserExist(userId: string): tUser | false {
   const exist = db.find((db_user) => db_user.id === userId);
 
   return !!exist ? exist : !!exist;
+}
+
+export function isValidUUID(uuid: string): boolean {
+  if (uuid.length !== 36) {
+    return false;
+  }
+  return validate(uuid);
+}
+
+export function getNewUUID(): string {
+  return uuidv4();
 }
