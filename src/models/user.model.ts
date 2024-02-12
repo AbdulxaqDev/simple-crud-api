df9a99e88681428457210a7818c514e36429e69e
AbdulxaqDev <abdulkhak8tursunov@gmail.com>
2024-02-12 18:56:33 +0300

    feat: add user model and http crud methods

---
+++ src/models/user.model.ts
@@ -0,0 +1,46 @@
+import db from "src/db";
+import { isUserExist } from "src/utils/user.util";
+
+import { tUser } from "src/types/user";
+
+// POST user
+export function addUser(newUser: tUser): boolean {
+  const user = isUserExist(newUser.id);
+  if (user) return false;
+
+  db.push(newUser);
+  return true;
+}
+
+// DELETE user
+export function removeUser(userId: string): boolean {
+  const user = isUserExist(userId);
+  if (!user) return false;
+
+  const index = db.indexOf(user);
+  db.splice(index, 1);
+  return true;
+}
+
+// PUT (update) user
+export function updateUser(updatedUser: tUser): boolean {
+  const user = isUserExist(updatedUser.id);
+  if (!user) return false;
+
+  const index = db.indexOf(user);
+  db[index] = updatedUser;
+  return true;
+}
+
+// GET user
+export function getUser(userId: string): tUser | false {
+  const user = isUserExist(userId);
+  if (!user) return false;
+
+  return user;
+}
+
+// GET users
+export function getUsers(): tUser[] {
+  return db;
+}
